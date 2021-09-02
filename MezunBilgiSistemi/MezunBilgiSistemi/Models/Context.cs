253c24341b1e37d49788098480a269e696cc0318
ferhatkpln <kaplanfrht123@gmail.com>
2021-09-03 01:00:18 +0300

    Mezun Bilgi Sistemi

---
+++ MezunBilgiSistemi/MezunBilgiSistemi/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using MezunBilgiSistemi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MezunBilgiSistemi.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=FERHATKAPLAN\\SQLEXPRESS;Database=MezunBilgiSistemi;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        public DbSet<Etkinlikler> Etkinliklers { get; set; }
+        public DbSet<Duyurular> Duyurulars { get; set; }
+        public DbSet<Haberler> Haberlers { get; set; }
+    }
+}
